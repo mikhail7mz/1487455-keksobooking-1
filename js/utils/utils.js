0f56c51188c0716e4109b2a41a3083f69c5e3fd6
Mikhail <65595208+mikhail7mz@users.noreply.github.com>
2023-08-02 17:15:32 +0300

    небольшой рефакторинг

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -10,6 +10,6 @@ const getRandomNumber = (min, max, digits) => +(Math.random() * (max - min) + mi
 
 const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
-const getRandomArraySlice = (elements) => elements.filter(() => (Math.random() - 0.5) > 0);
+const getRandomArraySlice = (elements) => elements.filter(() => Math.random() > 0.5);
 
 export { fillString, getRandomNumber, getRandomArrayElement, getRandomArraySlice };
