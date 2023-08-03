a778ca642a90b6f13f6ca2fbbebe7afe63c7872a
Mikhail <65595208+mikhail7mz@users.noreply.github.com>
2023-08-03 18:48:31 +0300

    добавлет проверки наличия данных для отрисовки

--- js/renderAdverts.js
+++ js/renderAdverts.js
@@ -12,36 +12,119 @@ const propertyTypeTitles = {
 };
 
 const mapCanvas = document.querySelector('#map-canvas');
+const popupTemplate = document.querySelector('#card').content.querySelector('.popup');
 
-const createFeatureTemplate = (feature) => `<li class="popup__feature popup__feature--${feature}"></li>`;
+const createFeaturesFragment = (features) => {
+  const featuresFragment = document.createDocumentFragment();
+  const featureTemplate = '<li class="popup__feature"></li>';
 
-const createPhotoTemplate = (url) => `<img src="${url}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`;
+  features.forEach((feature) => {
+    const newElement = createElement(featureTemplate);
+    newElement.classList.add(`popup__feature--${feature}`);
+    featuresFragment.append(newElement);
+  });
 
-const createListTemplate = (list, createItem) => list.reduce((template, item) => template + createItem(item), '');
+  return featuresFragment;
+};
+
+const createPhotosFragment = (photos) => {
+  const photosFragment = document.createDocumentFragment();
+  const photoTemplate = '<img class="popup__photo" width="45" height="40" alt="Фотография жилья">';
+
+  photos.forEach((photo) => {
+    const newElement = createElement(photoTemplate);
+    newElement.src = photo;
+    photosFragment.append(newElement);
+  });
+
+  return photosFragment;
+};
 
 const createAdvertTemplate = ({author, offer}) => {
   const {avatar} = author;
   const {title, address, price, type, rooms, guests, checkin, checkout, features, description, photos} = offer;
 
-  const featuresTemplate = (features) ? `<ul class="popup__features">${createListTemplate(features, createFeatureTemplate)}</ul>` : '';
-  const photosTemplate = (photos) ? `<div class="popup__photos">${createListTemplate(photos, createPhotoTemplate)}</div>` : '';
+  const popup = popupTemplate.cloneNode(true);
+  const avatarElement = popup.querySelector('.popup__avatar');
+  const titleElement = popup.querySelector('.popup__title');
+  const addressElement = popup.querySelector('.popup__text--address');
+  const priceElement = popup.querySelector('.popup__text--price');
+  const typeElement = popup.querySelector('.popup__type');
+  const capacityElement = popup.querySelector('.popup__text--capacity');
+  const timeElement = popup.querySelector('.popup__text--time');
+  const featuresElement = popup.querySelector('.popup__features');
+  const descriptionElement = popup.querySelector('.popup__description');
+  const photosElement = popup.querySelector('.popup__photos');
 
-  return `<article class="popup">
-    <img src="${avatar}" class="popup__avatar" width="70" height="70" alt="Аватар пользователя">
-    <h3 class="popup__title">${title}</h3>
-    <p class="popup__text popup__text--address">${address}</p>
-    <p class="popup__text popup__text--price">${price} <span>₽/ночь</span></p>
-    <h4 class="popup__type">${propertyTypeTitles[type]}</h4>
-    <p class="popup__text popup__text--capacity">${rooms} комнаты для ${guests} гостей</p>
-    <p class="popup__text popup__text--time">Заезд после ${checkin}, выезд до ${checkout}</p>
-    ${featuresTemplate}
-    <p class="popup__description">${description}</p>
-    ${photosTemplate}
-  </article>`;
+  avatarElement.src = avatar;
+
+  if (avatar) {
+    avatarElement.src = avatar;
+  } else {
+    avatarElement.remove();
+  }
+
+  if (title) {
+    titleElement.textContent = title;
+  } else {
+    titleElement.remove();
+  }
+
+  if (address) {
+    addressElement.textContent = address;
+  } else {
+    addressElement.remove();
+  }
+
+  if (price) {
+    priceElement.firstChild.textContent = `${price} `;
+  } else {
+    priceElement.remove();
+  }
+
+  if (type) {
+    typeElement.textContent = propertyTypeTitles[type];
+  } else {
+    typeElement.remove();
+  }
+
+  if (rooms || guests) {
+    capacityElement.textContent = `${rooms ? rooms : 0} комнаты для ${guests ? guests : 0} гостей`;
+  } else {
+    capacityElement.remove();
+  }
+
+  if (checkin || checkout) {
+    const checkinText = checkin ? `после ${checkin}` : 'в любое время';
+    const checkoutText = checkout ? `до ${checkout}` : 'в любое время';
+    timeElement.textContent = `Заезд ${checkinText}, выезд ${checkoutText}`;
+  } else {
+    timeElement.remove();
+  }
+
+  if (features && features.length) {
+    featuresElement.replaceChildren(createFeaturesFragment(features));
+  } else {
+    featuresElement.remove();
+  }
+
+  if (description) {
+    descriptionElement.textContent = description;
+  } else {
+    descriptionElement.remove();
+  }
+
+  if (photos && photos.length) {
+    photosElement.replaceChildren(createPhotosFragment(photos));
+  } else {
+    photosElement.remove();
+  }
+
+  return popup;
 };
 
 const renderAdvertById = (id) => {
-  mapCanvas.append(createElement(createAdvertTemplate(data[id])));
+  mapCanvas.append(createAdvertTemplate(data[id]));
 };
 
 export { renderAdvertById };
